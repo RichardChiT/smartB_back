621aca26eeb695532d63a36a0399d28d245907b9
RichardChiT <154022534+RichardChiT@users.noreply.github.com>
2024-05-15 14:33:50 -0700

    your commit message

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host: 'dpg-cp2i236v3ddc73cmkb70-a',
+    port: 5432,
+    user: 'smartbrain_u76t_user',
+    password: 'f0hDio6azzIrTGWhfbqrbOpyIXy90d9U',
+    database: 'smartbrain_u76t',
   }
 });
 
@@ -26,13 +27,15 @@ const app = express();
 app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res)=> { res.send(db.users) })
+// app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () =>{
+    console.log(`server started on port ${PORT}`)
 })
